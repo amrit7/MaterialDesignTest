2b81f6f923545dd33d04b0d389d20f333ac4bdd8
amrit <amrit@DESKTOP-Q9AQQ93>
2024-03-08 18:09:22 +0000

    Add project files.

---
+++ ExcelDNATest/MyFunctions.cs
@@ -0,0 +1,16 @@
+﻿using ExcelDna.Integration;
+
+namespace ExcelDNATest;
+
+
+public class MyFunctions
+{
+    [ExcelFunction(Description="My first .NET function")]
+    public static string SayHello(string name)
+    {
+        //var frm = new MainWindow();
+        //frm.show()
+
+        return "Hello " + name;
+    }
+}
