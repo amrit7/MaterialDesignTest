a3c0b621662040751ef590438fb97272349c0155
A <amrit7@hotmail.com>
2024-03-19 17:06:50 +0000

    Test PostAsJsonAsync - fail / success

--- WpfTest/HttpPostCall.cs
+++ WpfTest/HttpPostCall.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
+﻿using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
 
 namespace WpfTest;
 
 public class HttpPostCall
 {
+    private readonly string _baseUrl = "";
+
     private static HttpClient _httpClient = new HttpClient();
 
     public HttpPostCall()
@@ -19,6 +19,21 @@ public class HttpPostCall
         _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("No Authorization");
 
+        var currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var configFileFullPath = Path.Combine(currentDirectory, @"config\config.json");
+
+        // **************
+        // with the below lines and package Microsoft.Extensions.Configuration.Json 
+        // the SetTestValue call fails
+        // if you comment the below lines and remove the package
+        // the SetTestValue call is successful
+        // **************
+
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile(configFileFullPath)
+            .Build();
+
+        _baseUrl = configuration["baseUrl"];
     }
 
     public static async Task<bool> SetTestValue()
--- WpfTest/MainWindow.xaml.cs
+++ WpfTest/MainWindow.xaml.cs
@@ -1,44 +1,19 @@
-﻿using Microsoft.Xaml.Behaviors.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace WpfTest
+namespace WpfTest;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private MainWindowViewModel _viewModel { get; }
+
+    public MainWindow()
     {
-        private MainWindowViewModel _viewModel { get; }
+        _viewModel = new MainWindowViewModel();
+        DataContext = _viewModel;
 
-        public MainWindow()
-        {
-            _viewModel = new MainWindowViewModel();
-            DataContext = _viewModel;
-
-            InitializeComponent();
-        }
-        
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            TestPost();
-        }
-
-        public async void TestPost()
-        {
-            var test = await HttpPostCall.SetTestValue();
-        }
+        InitializeComponent();
     }
 }
